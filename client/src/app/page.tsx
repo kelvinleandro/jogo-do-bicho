990b004ec09d6ea4b404fa485dae0c2cbfbb33d4
kelvinleandro <kelvinleandro5@gmail.com>
2024-08-15 09:40:22 -0300

    feat: initialize game page and fix error on disconnect

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,31 +1,14 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
-import { VolumeX, Volume2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  const audioRef = useRef<HTMLAudioElement>(null);
-  const [isMuted, setIsMuted] = useState(false);
+  const router = useRouter();
 
-  useEffect(() => {
-    // Play the audio when the component mounts
-    if (audioRef.current) {
-      audioRef.current.play();
-    }
-  }, []);
-
-  const toggleMute = () => {
-    if (audioRef.current) {
-      audioRef.current.muted = !audioRef.current.muted;
-      setIsMuted(audioRef.current.muted);
-    }
-  };
-  
   return (
     <div className="bg-slate-800 w-full h-screen py-8 flex flex-col md:flex-row justify-start md:justify-center items-center space-y-4 md:space-x-4">
-      {/* <audio ref={audioRef} src="/home-sound.mp3" loop /> */}
       <Image
         src="https://c.tenor.com/NVP2kRD7CHsAAAAC/tenor.gif"
         alt="dog dancing"
@@ -33,11 +16,16 @@ export default function Home() {
         height={200}
         unoptimized
       />
-      <h1 className="text-5xl font-bold text-white">Jogo do Bicho</h1>
-      <Button onClick={() => {}} className="text-xl" variant="secondary">Start Game</Button>
-      <Button className="absolute top-0 right-2" onClick={toggleMute} asChild variant="secondary" size="icon">
-        {isMuted ? <VolumeX /> : <Volume2 />}
-      </Button>
+      <div className="flex flex-col items-center space-y-4">
+        <h1 className="text-5xl font-bold text-white">Jogo do Bicho</h1>
+        <Button
+          onClick={() => router.push("/game")}
+          className="text-xl"
+          variant="secondary"
+        >
+          Start Game
+        </Button>
+      </div>
     </div>
   );
 }
--- server/app.py
+++ server/app.py
@@ -1,42 +1,15 @@
 from flask import Flask, request
 from flask_socketio import SocketIO, emit, disconnect
 import random
+from animals import ANIMAIS
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 MAX_CLIENTS = 2
+allow_new_clients = True
 clients = {}
 guesses = {}
-
-# Mapeamento dos animais para os números
-ANIMAIS = {
-    "avestruz": [1, 2, 3, 4],
-    "burro": [5, 6, 7, 8],
-    "cachorro": [9, 10, 11, 12],
-    "leão": [13, 14, 15, 16],
-    "macaco": [17, 18, 19, 20],
-    "pavão": [21, 22, 23, 24],
-    "peru": [25, 26, 27, 28],
-    "tigre": [29, 30, 31, 32],
-    "urso": [33, 34, 35, 36],
-    "elefante": [37, 38, 39, 40],
-    "cobra": [41, 42, 43, 44],
-    "coelho": [45, 46, 47, 48],
-    "cavalo": [49, 50, 51, 52],
-    "rato": [53, 54, 55, 56],
-    "boi": [57, 58, 59, 60],
-    "camelo": [61, 62, 63, 64],
-    "gato": [65, 66, 67, 68],
-    "galo": [69, 70, 71, 72],
-    "jacaré": [73, 74, 75, 76],
-    "javali": [77, 78, 79, 80],
-    "peixe": [81, 82, 83, 84],
-    "ressaca": [85, 86, 87, 88],
-    "sapo": [89, 90, 91, 92],
-    "tartaruga": [93, 94, 95, 96],
-    "vaca": [97, 98, 99, 100]
-}
 
 @app.route('/')
 def index():
@@ -44,20 +17,28 @@ def index():
 
 @socketio.on('connect')
 def handle_connect():
-    if len(clients) < MAX_CLIENTS:
+    global allow_new_clients
+    if len(clients) < MAX_CLIENTS and allow_new_clients:
         clients[request.sid] = {'guess': None}
         print(f'Jogador conectado. Total de jogadores: {len(clients)}')
 
         if len(clients) == MAX_CLIENTS:
-            start_game()
+            allow_new_clients = False
+            socketio.emit('start_game')
+            start_new_round()
     else:
         emit('message', 'Número máximo de jogadores atingido. Desconectando...')
         disconnect()
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    clients.pop(request.sid, None)
-    print(f'Jogador desconectado. Total de jogadores: {len(clients)}')
+    try:
+        clients.pop(request.sid, None)
+        guesses.pop(request.sid, None)
+        print(f'Jogador desconectado. Total de jogadores: {len(clients)}')
+
+    except Exception as e:
+        print(f'Erro ao desconectar jogador: {e}')
 
 @socketio.on('guess')
 def handle_guess(data):
@@ -65,30 +46,32 @@ def handle_guess(data):
     guesses[request.sid] = animal_guess
     print(f"Jogador {request.sid} escolheu: {animal_guess}")
 
-    if len(guesses) == MAX_CLIENTS:
+    if len(guesses) == len(clients):
         evaluate_results()
 
-def start_game():
+def start_new_round():
     global chosen_animal, chosen_numbers
     chosen_animal = random.choice(list(ANIMAIS.keys()))
     chosen_numbers = ANIMAIS[chosen_animal]
     print(f"Animal selecionado: {chosen_animal} ({chosen_numbers})")
-    socketio.emit('message', 'START GAME')
-    socketio.emit('message', 'GUESS ANIMAL')
+    socketio.emit('guess_animal')
 
 def evaluate_results():
     for sid, animal in guesses.items():
-        if animal == chosen_animal:
-            emit('message', 'Você acertou!', room=sid)
-        else:
-            emit('message', f'Você errou! O animal correto era {chosen_animal}.', room=sid)
+        try:
+            if animal == chosen_animal:
+                emit('guess_result', f'ACERTOU {chosen_animal}', room=sid)
+            else:
+                emit('guess_result', f'ERROU {chosen_animal}.', room=sid)
+        except Exception as e:
+            print(f'Erro ao enviar resultado para jogador {sid}: {e}')
     
     # Limpa apenas as adivinhações para permitir uma nova rodada
     guesses.clear()
 
     # Inicia uma nova rodada automaticamente
     socketio.emit('message', 'Nova rodada iniciando...')
-    start_game()
+    start_new_round()
 
 if __name__ == '__main__':
-    socketio.run(app, port=3000)
+    socketio.run(app, port=5000)
